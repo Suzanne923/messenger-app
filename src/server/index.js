8e76ca90f75125cf944d2df7b389eab54e514022
Suzanne Corporaal <suzannecorporaal@gmail.com>
2018-01-26 22:41:36 +0100

    first version

--- src/server/index.js
+++ src/server/index.js
@@ -32,6 +32,6 @@ module.exports.io = io;
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect('MONGODB_URI');
 
-/*app.get('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/../../build/index.html');
-})*/
+})
