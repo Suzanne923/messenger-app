5a3edb8ce60dd9d856ccd6d9495cab9fb6ab7019
Suzanne Corporaal <suzannecorporaal@gmail.com>
2018-01-27 22:16:35 +0100

    resolving forbidden error

--- src/server/router.js
+++ src/server/router.js
@@ -7,7 +7,7 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
-  app.get('/chatbox', requireAuth, function(request, response) {
+  /*app.get('/chatbox', requireAuth, function(request, response) {
     console.log('require auth: ', requireAuth);
     //response.sendFile(path.resolve(__dirname + '/../../build/index.html'));
     response.sendStatus(500);
@@ -17,7 +17,7 @@ module.exports = function(app) {
   });
   app.get('/register', function(request, response) {
     response.sendFile(path.resolve(__dirname + '/../../build/index.html'));
-  });
+  });*/
   app.post('/login', requireLogin, Authentication.login);
   app.post('/register', Authentication.register);
 }
