1aea7ca08bc892cc632a9374b640673c79c997fe
Suzanne Corporaal <suzannecorporaal@gmail.com>
2018-01-27 13:11:39 +0100

    iwhfiewhf

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,7 +12,7 @@ import {
   UPDATE_TYPING_IN_CHAT
 } from './types';
 
-const ROOT_URL = '/';
+const ROOT_URL = `${window.location.protocol}//${window.location.hostname}`;
 
 export function loginUser({ username, password }) {
   return function(dispatch) {
